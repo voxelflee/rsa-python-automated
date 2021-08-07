153c2af7a16eb3a63ad3b97255fc3c4e018d0db6
voxelflee <ddifdevnull@gmail.com>
2021-08-07 05:42:46 +0300

    Converted tabs to 4 spaces in remaining files

--- rsa_gen_primes.py
+++ rsa_gen_primes.py
@@ -1,24 +1,24 @@
 def findPrimes(characters_min, characters_max, maximum_primes='0'):
-	maximum_primes_set = False
-	if maximum_primes != 0: maximum_primes_set = True
-	primes_list = []
-	def isPrime(number):
-		if number > 1:
-			for discriminator in range(2, int(number**0.5) + 1):
-				if (number % discriminator) == 0:
-					return False
-			return True
-		elif number == 1: return True
-	start_point = '1'
-	end_point = '9'
-	for _ in range(characters_min - 1):
-		start_point += '0'
-	for _ in range(characters_max - 1):
-		end_point += '9'
-	start_point = int(start_point)
-	end_point = int(end_point)
-	for prime_candidate in range(start_point, end_point + 1):
-		if maximum_primes_set:
-			if len(primes_list) == maximum_primes: break
-		if isPrime(prime_candidate): primes_list.append(prime_candidate)
-	return(primes_list)
+    maximum_primes_set = False
+    if maximum_primes != 0: maximum_primes_set = True
+    primes_list = []
+    def isPrime(number):
+        if number > 1:
+            for discriminator in range(2, int(number**0.5) + 1):
+                if (number % discriminator) == 0:
+                    return False
+            return True
+        elif number == 1: return True
+    start_point = '1'
+    end_point = '9'
+    for _ in range(characters_min - 1):
+        start_point += '0'
+    for _ in range(characters_max - 1):
+        end_point += '9'
+    start_point = int(start_point)
+    end_point = int(end_point)
+    for prime_candidate in range(start_point, end_point + 1):
+        if maximum_primes_set:
+            if len(primes_list) == maximum_primes: break
+        if isPrime(prime_candidate): primes_list.append(prime_candidate)
+    return(primes_list)
